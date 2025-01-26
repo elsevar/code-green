1981550ae32d76e90bd50c3446a366d65a1fc5f2
elsevar <elsever1@live.com>
2025-01-26 20:49:48 +0100

    Add validation

--- emission/serializers.py
+++ emission/serializers.py
@@ -5,11 +5,24 @@ from .services.EmssionCalcService.dataclasses import EnergyConsumption
 class EnergyConsumptionSerializer(serializers.Serializer):
     description = serializers.CharField(max_length=255)
     energy_source_id = serializers.CharField(max_length=100)
-    energy_consumption = serializers.FloatField()
-    emission_factor = serializers.FloatField(required=False, allow_null=True)
+    energy_consumption =  serializers.DecimalField(
+        max_digits=15,
+        decimal_places=5,
+        coerce_to_string=False) 
+    emission_factor = serializers.DecimalField(
+        max_digits=10,
+        decimal_places=5,
+        required=False,
+        allow_null=True,
+        coerce_to_string=False)
 
     def create(self, validated_data):
         """
         Create and return a new `EnergyConsumption` instance, given the validated data.
         """
-        return EnergyConsumption(**validated_data)
+        return EnergyConsumption(
+            description=validated_data['description'],
+            energy_source_id=validated_data['energy_source_id'],
+            energy_consumption=float(validated_data['energy_consumption']),
+            emission_factor=float(validated_data['emission_factor']) if validated_data.get('emission_factor') is not None else None
+        )
